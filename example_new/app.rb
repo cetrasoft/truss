7581777794fb634df3d4a005ae72a5ff0dbaafd9
Carlos Ramirez III <carlos@cetrasoft.com>
2012-02-03 13:49:40 -0500

    Removed headers from font module. Renamed font module classes and added configuration for sizes. Added kss docs

--- example_new/app.rb
+++ example_new/app.rb
@@ -10,12 +10,19 @@ get '/media' do
   erb :styleguide
 end
 
+get '/font' do
+  @sizes = [['1.1.1', 'f-xxs'], ['1.1.2', 'f-xs'], ['1.1.3', 'f-s'], ['1.1.4', 'f-m'], ['1.1.5', 'f-l'], ['1.1.6', 'f-xl'], ['1.1.7', 'f-xxl']]
+  @styleguide = Kss::Parser.new('public/stylesheets')
+  erb :font
+end
+
 helpers do
   # Generates a styleguide block. A little bit evil with @_out_buf, but
   # if you're using something like Rails, you can write a much cleaner helper
   # very easily.
   def styleguide_block(section, &block)
     @section = @styleguide.section(section)
+    @test = block.inspect
     @example_html = capture{ block.call }
     @_out_buf << erb(:_styleguide_block)
   end
